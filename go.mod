55f35ee96daf751794cb125b8ffc7a57c5bb7144
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2023-03-23 18:12:07 +0100

    Add code using greetings

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sergiofgonzalez/greeter
 
 go 1.20
+
+require github.com/sergiofgonzalez/greetings v0.0.0-20230323074716-92b19ea24277 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sergiofgonzalez/greetings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(greetings.Hello())
 }
