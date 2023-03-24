f3b65a0f00aaaa471ecf4852fc8ff5c60f68181a
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2023-03-24 17:57:56 +0100

    Add quotes library usage

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sergiofgonzalez/greeter
 
 go 1.20
+
+require github.com/sergiofgonzalez/quotes v0.0.0-20230324162752-2162e4a7369c // indirect
--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sergiofgonzalez/quotes"
+	"github.com/sergiofgonzalez/quotes/computers"
+	"github.com/sergiofgonzalez/quotes/poetry"
+	"github.com/sergiofgonzalez/quotes/tv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	fmt.Println("=== Random quotes")
+	for i := 0; i < 5; i++ {
+		fmt.Println(quotes.Quote())
+	}
+
+	fmt.Println("\n=== Poetry")
+	fmt.Println(poetry.Quote())
+
+	fmt.Println("\n=== Tv")
+	fmt.Println(tv.Quote())
+	fmt.Println(tv.Quote())
+
+	fmt.Println("\n=== Computers")
+	for i := 0; i < 10; i++ {
+		fmt.Println(computers.Quote())
+	}
 }
